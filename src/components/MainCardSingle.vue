9573cc8c2d4a210dbf3931b47d157f0a9f1f417a
zflorezz <zakinahf030904@gmail.com>
2024-02-11 16:00:43 -0600

    Card data and style done

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -1,9 +1,22 @@
 <script setup>
 import { faker } from '@faker-js/faker'
 const fullname = faker.person.fullName()
+const firstname = faker.person.firstName()
+const lastname = faker.person.lastName()
+const job_title = faker.person.jobTitle()
+const bio = faker.lorem.lines(2)
+const username = faker.internet.userName({firstName: firstname, lastName:lastname}).toLowerCase()
+
 </script>
 
 <template>
-
-    {{ fullname }} <br />
+    <!-- {{ fullname }} <br /> -->
+    <div class="border border-gray-400 rounded-lg shadow bg-gray-200 h-full">
+        <img :src="faker.image.urlLoremFlickr({category: 'cat'})" />
+        <div class="p-4">
+            <p class="mb-2 text-2xl font-bold">{{ fullname }}</p>
+            <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
+            <p class="mb-3 font-normal text-gray-700">{{ bio }}</p>
+        </div>
+    </div>
 </template>
--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -4,7 +4,9 @@ import MainCardSingle from './MainCardSingle.vue';
 </script>
 
 <template>
+<div class="grid grid-cols-5 gap-10 p-10">
+    <MainCardSingle v-for="card in 20" :key="card"></MainCardSingle>
 
-<MainCardSingle v-for="card in 20" :key="card"></MainCardSingle>
+</div>
 
 </template>
