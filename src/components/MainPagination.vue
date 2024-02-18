eb2c99d0638c776207088ee376e0031f46ca67c2
zflorezz <zakinahf030904@gmail.com>
2024-02-17 23:18:48 -0600

    added style to pagination

--- src/components/MainPagination.vue
+++ src/components/MainPagination.vue
@@ -1,11 +1,11 @@
 <template>
-<div>
+<div class="flex justify-center gap-4 p-5">
 
-    <button @click="prevPage" :disabled="activePage === 1">PREV</button>
+    <button @click="prevPage" :disabled="activePage === 1" class="rounded-md bg-purple-300 p-2 font-medium text-slate-700 shadow-md-">PREV</button>
    
-    <button v-for="page in pages" :key="page" class=""> {{ page }}</button>
+    <button v-for="page in pages" :key="page" class="rounded-md bg-pink-100 p-2 font-medium text-slate-700 shadow-md-" :class="page === activePage ? 'bg-pink-300 text-slate-700': ''"> {{ page }}</button>
    
-    <button @click="nextPage" :disabled="activePage === pages">NEXT</button>
+    <button @click="nextPage" :disabled="activePage === pages" class="rounded-md bg-purple-300 p-2 font-medium text-slate-700 shadow-md">NEXT</button>
 
 </div>
 
