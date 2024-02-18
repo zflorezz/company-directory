4bf295597b3be73b859a45337d1cf10e8a6c23ee
zflorezz <zakinahf030904@gmail.com>
2024-02-17 23:00:18 -0600

    pagination working

---
+++ src/components/MainPagination.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+
+    <button @click="prevPage" :disabled="activePage === 1">PREV</button>
+   
+    <button v-for="page in pages" :key="page" class=""> {{ page }}</button>
+   
+    <button @click="nextPage" :disabled="activePage === pages">NEXT</button>
+
+</div>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const pages = ref(10)
+const activePage = ref(2)
+
+const prevPage = () => {
+    if (activePage.value > 1) {
+        activePage.value--
+    }
+
+    console.log(activePage.value)
+}
+
+const nextPage = () => {
+    if (activePage.value < pages.value) {
+        activePage.value++
+    }
+
+    console.log(activePage.value)
+}
+
+
+
+</script>
+
+
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,8 +2,10 @@
 //import { ref } from 'vue'
 //const title = ref('Hello There ✋')
 import MainCards from '@/components/MainCards.vue';
+import MainPagination from '@/components/MainPagination.vue';
 </script>
 
 <template>
   <MainCards></MainCards>
+  <MainPagination></MainPagination>
 </template>
